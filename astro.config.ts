24febbd375c7b5c43138648951f150658178e913
Salah Rashad <salah.r.ahmed@gmail.com>
2025-08-02 16:35:43 +0300

    fix conflicts

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,45 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import starlight from "@astrojs/starlight";
+import starlightThemeObsidian from "starlight-theme-obsidian";
 
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel({
     imageService: true,
     webAnalytics: {
       enabled: true,
     },
   }),
+  integrations: [
+    starlight({
+      title: "salah.dev",
+      plugins: [starlightThemeObsidian()],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/salah-rashad/",
+        },
+        {
+          icon: "linkedin",
+          label: "LinkedIn",
+          href: "https://www.linkedin.com/in/salah-rashad/",
+        },
+      ],
+      // sidebar: [
+      //   {
+      //     label: "Guides",
+      //     items: [
+      //       // Each item here is one entry in the navigation menu.
+      //       { label: "Example Guide", slug: "guides/example" },
+      //     ],
+      //   },
+      //   {
+      //     label: "Reference",
+      //     autogenerate: { directory: "reference" },
+      //   },
+      // ],
+    }),
+  ],
 });
